c9b072cbe060b6b329d8836159a82f69d942b206
Elowan Premel <elowanpremel@MacBook-Pro-de-Elowan.local>
2026-04-29 17:35:42 +0200

    Add Maison Rose photo pass to key pages

--- assets/site.js
+++ assets/site.js
@@ -15,7 +15,7 @@ const DEFAULT_SITE_SETTINGS = {
   instagram_url: "https://www.instagram.com/lamaisonrosedewallerand/",
   helloasso_url: "https://www.helloasso.com/associations/la-maison-rose-de-wallerand",
   helloasso_organization_slug: "la-maison-rose-de-wallerand",
-  home_hero_image: "assets/official-house.jpg",
+  home_hero_image: "/assets/uploads/maison-rose-facade-hero.jpg",
   helloasso_checkout_membership_item_name: "Adhesion individuelle a La Maison Rose de Wallerand",
   helloasso_checkout_membership_amount: 20,
   helloasso_checkout_membership_couple_item_name: "Adhesion couple a La Maison Rose de Wallerand",
